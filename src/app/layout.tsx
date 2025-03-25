c78df1bed182607d47714062f722347bebdbc50b
James_Victor <112592627+JamesVictor-O@users.noreply.github.com>
2025-03-25 04:29:44 +0100

    Feat header (#14)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Footer from "@/components/organisms/footer/Footer";
+import Navbar from "@/components/organisms/header/Header";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Navbar/>
         {children}
         <Footer />
       </body>
