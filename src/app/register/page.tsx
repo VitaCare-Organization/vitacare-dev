38aed544a685742f151cc5e6151754faa5bcab0f
Luis Rojas <luis.4.2000@hotmail.com>
2025-04-24 19:51:21 -0600

    feat: add contracts folder

--- src/app/register/page.tsx
+++
@@ -1,5 +0,0 @@
-import PatientRegistrationPage from "@/components/pages/patient-registration/patient-registration-page"
-
-export default function Register() {
-  return <PatientRegistrationPage />
-}
--- src/components/atoms/Icon/Icon.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-interface IconProps {
-  children: ReactNode;
-}
-
-export default function Icon({ children }: IconProps) {
-  return <div className="text-blue-500 text-3xl">{children}</div>;
-}
