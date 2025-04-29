f67da26c05e6e608264bf239082c78aa06a47ac1
Bosun <96661657+Bosun-Josh121@users.noreply.github.com>
2025-04-29 04:24:03 +0100

    Feature: Appointment Scheduling Contract (#37)

---
+++ apps/vitacare-contracts/contracts/appointment-scheduling/src/lib.rs
@@ -0,0 +1,11 @@
+#![no_std]
+mod storage_types;
+mod error;
+mod contract;
+
+pub use contract::AppointmentSchedulingContract;
+pub use error::Error;
+pub use storage_types::{Appointment, AppointmentStatus};
+
+#[cfg(test)]
+mod test;
