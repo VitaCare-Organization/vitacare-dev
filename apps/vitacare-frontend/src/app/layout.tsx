9a6d01fa2e91ca1c5148e26c4e5a754eeea548ca
James Victor <112592627+JamesVictor-O@users.noreply.github.com>
2025-06-05 04:28:35 +0100

    Implement User Context with TypeScript and Tests (#63)

--- apps/vitacare-frontend/src/app/layout.tsx
+++ apps/vitacare-frontend/src/app/layout.tsx
@@ -1,26 +1,28 @@
-
 import type { Metadata } from "next";
 import "./globals.css";
 import Footer from "@/components/organisms/Footer/Footer";
 import Navbar from "@/components/organisms/Header/Header";
+import { UserProvider } from "@/context/userContext";
 
 export const metadata: Metadata = {
   title: "VitaCare - Patient Registration",
   description: "Register to access the VitaCare platform",
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>
-        <Navbar/>
-        {children}
-        <Footer />
+        <UserProvider>
+          <Navbar />
+          {children}
+          <Footer />
+        </UserProvider>
       </body>
     </html>
-  )
+  );
 }
---
+++ apps/vitacare-frontend/src/context/userContext.tsx
@@ -0,0 +1,44 @@
+import { createContext, ReactNode, useContext, useState } from "react";
+
+export enum UserRole {
+  Doctor = "Doctor",
+  Patient = "Patient",
+  Hospital = "Hospital",
+  Admin = "Admin",
+}
+
+interface User {
+  email: string;
+  userId: string;
+  publicKey: string;
+  role: UserRole;
+}
+
+interface UserContextType {
+  user: User | null;
+  setUser: (user: User | null) => void;
+}
+
+interface UserProviderProps {
+  children: ReactNode;
+}
+
+const UserContext = createContext<UserContextType | undefined>(undefined);
+
+export const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUser = (): UserContextType => {
+  const context = useContext(UserContext);
+  if (!context) {
+    throw new Error("useUser must be used within a UserProvider");
+  }
+  return context;
+};
