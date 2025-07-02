8fbd10de1cfdbc4199bf6f29633d33e9dec62313
Ugochukwu Nebolisa <nebolisaugochukwu@gmail.com>
2025-07-02 15:42:02 +0100

    fix: completed the login page

--- apps/vitacare-frontend/src/schemas/LoginFormSchema.ts
+++ apps/vitacare-frontend/src/schemas/LoginFormSchema.ts
@@ -1,8 +1,11 @@
 import { z } from "zod";
 
 export const LoginFormSchema = z.object({
-  email: z.string().email("Invalid email format"),
-  password: z.string().min(8, "Password must be at least 8 characters long"),
+  email: z.string().nonempty("Email is required").email("Invalid email format"),
+  password: z
+    .string()
+    .nonempty("Password is required")
+    .min(8, "Password must be at least 8 characters long"),
   accountType: z.enum(["Patient", "Doctor", "Hospital"], {
     required_error: "Please select an account type",
   }),
